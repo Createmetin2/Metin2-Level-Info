9814326a7526670e104a16f7e128e0642f2b9dd8
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-03-30 12:31:26 +0300

    Type update

--- 1.Svn/Server/game/src/questlua_global.cpp
+++ 1.Svn/Server/game/src/questlua_global.cpp
@@ -2,13 +2,15 @@
 #if defined(LEVEL_INFO)
 	int _add_level_info(lua_State* L)
 	{
-		if (!lua_isnumber(L, 1) || !lua_isstring(L, 2))
+		if (!lua_isnumber(L, 1) || !lua_isstring(L, 2) || !lua_isnumber(L, 3))
 			return 0;
 
 		const auto level = static_cast<int>(lua_tonumber(L, 1));
 		const auto msg = std::string(lua_tostring(L, 2));
-		if (!msg.empty() && level > 0)
-			LvlInfoMap.emplace(level, msg);
+		const auto type = static_cast<BYTE>(lua_tonumber(L, 3));
+		
+		if (!msg.empty() && level > 0 && type >= CHAT_TYPE_TALKING && type < CHAT_TYPE_MAX_NUM)
+			LvlInfoMap.emplace(level, std::make_pair(msg, type));
 
 		return 0;
 	}
