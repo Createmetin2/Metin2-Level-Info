0682b12566fad19a85d8f2d3c98cb30222ee5a25
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-03-30 12:31:03 +0300

    Type update

--- 1.Svn/Server/game/src/config.h
+++ 1.Svn/Server/game/src/config.h
@@ -2,5 +2,5 @@
 
 #if defined(LEVEL_INFO)
 #include <unordered_map>
-extern std::unordered_multimap<int, std::string> LvlInfoMap;
+extern std::unordered_multimap<int, std::pair<std::string, BYTE>> LvlInfoMap;
 #endif
