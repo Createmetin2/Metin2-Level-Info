2f3072423c3b3106336cf2c1897d5e3b1dc1ac82
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-03-30 12:31:54 +0300

    Type update

--- 2.Server/locale/xx/settings.lua
+++ 2.Server/locale/xx/settings.lua
@@ -1,5 +1,5 @@
 -- Add
--- level, msg (you can add multiple msg)
-add_level_info(2, "MSGGGG11111");
-add_level_info(2, "MSGGGG122");
-add_level_info(3, "MSGGGG122333");
+-- level, msg, type (you can add multiple msg)
+add_level_info(2, "MSGGGG1", 8); --8 big notice
+add_level_info(2, "MSGGGG122", 2); --2 notice
+add_level_info(3, "MSGGGG122333", 1); --1 info
